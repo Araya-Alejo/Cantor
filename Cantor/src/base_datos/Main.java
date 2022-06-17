bc07e74024fb4d6269b414400d7a2e6fcdbd7e1f
rodrigox.vargas@alumnos.frm.utn.edu.ar <rodrigox.vargas@alumnos.frm.utn.edu.ar>
2022-06-16 23:04:57 -0300

    asdasd

--- Cantor/src/base_datos/Main.java
+++ Cantor/src/base_datos/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         
-        
+        Conector con = new Conector();
+        con.connect();
+        con.close();
         
         
     }
