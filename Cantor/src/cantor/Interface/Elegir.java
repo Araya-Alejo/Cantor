20987144417af16f8bb7dd09553657dc33a911b4
Araya-Alejo <alejo.araya@alumnos.frm.utn.edu.ar>
2022-06-25 20:18:33 -0300

    asd

--- Cantor/src/cantor/Interface/Elegir.java
+++ Cantor/src/cantor/Interface/Elegir.java
@@ -192,8 +192,9 @@ public class Elegir extends javax.swing.JFrame {
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(_salida_canto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
-                .addComponent(jButton1))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -204,9 +205,7 @@ public class Elegir extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -240,15 +239,18 @@ public class Elegir extends javax.swing.JFrame {
                         for (Gallo gallo : gallos) {
                             if (_artistas.getSelectedItem().equals(gallo.nombre)) {
                                 _salida_nacimiento.setText(String.valueOf(gallo.fechaNacimiento));
-                                _salida_instrumento.setText("--------------------------------");
                                 _salida_canto.setText(gallo.cuando.tipo);
                             }
                         }
                     }
                     if (jComboBox1.getSelectedIndex() == 3) {
                         ArrayList<Canario> canarios = conn.getGestorCanario().getDatos();
-                        String[] arreglo3 = convertirArrayArregloCanario(canarios);
-                        this._artistas.setModel(new DefaultComboBoxModel(arreglo3));
+                        for (Canario canario : canarios) {
+                            if (_artistas.getSelectedItem().equals(canario.nombre)) {
+                                _salida_nacimiento.setText(String.valueOf(canario.fechaNacimiento));
+                                _salida_canto.setText(canario.cuando.tipo);
+                            }
+                        }
                     }
                 } else {
                     String[] arreglo = {""};
